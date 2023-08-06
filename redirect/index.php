bffe2763da5275f07ae07bcf183a676c350f4e1f
elemermelada <iiasanmi9@gmail.com>
2023-08-06 23:35:18 +0200

    fixed new ip

--- redirect/index.php
+++ redirect/index.php
@@ -5,7 +5,7 @@ $ip = substr($data,0,strpos($data,","));
 $path = $_GET['path'];
 
 if ($ip==$_SERVER['REMOTE_ADDR']) {
-	$ip='192.168.0.4';
+	$ip='192.168.1.4';
 }
 
 echo '<meta http-equiv="refresh" content="0;url=http://' . $ip . '/' . $path . '">';
