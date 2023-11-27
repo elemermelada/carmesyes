10bd27e9e0c20413fefda278cb72b69b35aeccb3
elemermelada <iiasanmi9@gmail.com>
2023-11-27 17:05:57 +0100

    simple second device support

--- index.php
+++ index.php
@@ -6,9 +6,20 @@ if ($_GET['pass']=="lolaso") {
 	echo $_SERVER["REMOTE_ADDR"];
 
 }
+else if ($_GET['pass']=="lolmao") {
+
+	file_put_contents('log2', $_SERVER['REMOTE_ADDR'] . ", " . date("H:i d-m"));
+	echo $_SERVER["REMOTE_ADDR"];
+
+}
 else {
 	
 	$data = file_get_contents('log');
+	$ip = substr($data,0,strpos($data,","));
+	$time = substr($data,strpos($data,","));
+	echo '<a href="http://' . $ip . '">' . $ip . '</a>' . $time . ' <br>';
+
+	$data = file_get_contents('log2');
 	$ip = substr($data,0,strpos($data,","));
 	$time = substr($data,strpos($data,","));
 	echo '<a href="http://' . $ip . '">' . $ip . '</a>' . $time . ' <br>';
