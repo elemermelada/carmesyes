87befc7453b80e0af1308c41d5818e94e6ed4f01
elemermelada <iiasanmi9@gmail.com>
2023-11-28 19:37:29 +0100

    trying stuff

--- db.php
+++ db.php
@@ -11,7 +11,9 @@ function db_connect()
     $link = new mysqli($host_name, $user_name, $password, $database);
 
     if ($link->connect_error) {
-        return null;
+        die('<p>Error al conectar con servidor MySQL: ' . $link->connect_error . '</p>');
+    } else {
+        echo '<p>Se ha establecido la conexión al servidor MySQL con éxito.</p>';
     }
 
     return $link;
