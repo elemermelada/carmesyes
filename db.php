3179f0a1eb62f554c28a203f0972218d5934f908
elemermelada <iiasanmi9@gmail.com>
2023-11-28 19:26:43 +0100

    Adding db support

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+function db_connect()
+{
+
+    $config = json_decode(file_get_contents("db_config.json"), true);
+    $host_name = $config['host_name'];
+    $database = $config['database'];
+    $user_name = $config['user_name'];
+    $password = $config['password'];
+
+    $link = new mysqli($host_name, $user_name, $password, $database);
+
+    if ($link->connect_error) {
+        return null;
+    }
+
+    return $link;
+}
+?>
---
+++ ping/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Connect to DB
+require '../db.php';
+$link = db_connect();
+
+// Grab parameters
+$device_id = $_GET['id'];
+$device_ip = $_SERVER['REMOTE_ADDR'];
+$date = new DateTime();
+
+// TODO - implement better SQL injection prevention measures??
+if (!is_numeric($device_id)) {
+    http_response_code(400);
+    exit();
+}
+
+// Perform request
+$query = 'INSERT INTO `carmesyes_pinglog` (`date`, `ip`, `device`) VALUES (CURRENT_TIME(), \'' . $device_ip . '\', \'' . $device_id . '\');';
+?>
