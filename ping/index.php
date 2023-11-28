07941aded6cc0cf6d7b0fc3f9459521f8c9cde08
elemermelada <iiasanmi9@gmail.com>
2023-11-28 20:18:39 +0100

    trying stuff

--- ping/index.php
+++ ping/index.php
@@ -3,8 +3,12 @@
 error_reporting(E_ALL);
 
 // Connect to DB
-require '../db.php';
-$link = db_connect();
+$config = json_decode(file_get_contents("db_config.json"), true);
+$host_name = $config['host_name'];
+$database = $config['database'];
+$user_name = $config['user_name'];
+$password = $config['password'];
+$link = new mysqli($host_name, $user_name, $password, $database);
 
 // Grab parameters
 $device_id = $_GET['id'];
