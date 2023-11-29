0763828946de768cbbc0db23d41c21335f194ce6
elemermelada <iiasanmi9@gmail.com>
2023-11-29 02:16:25 +0100

    responsive UI

--- assets/main.js
+++ assets/main.js
@@ -7,7 +7,15 @@ function reactive_ui() {
     y = win.innerHeight || docElem.clientHeight || body.clientHeight;
   if (x < y) {
     document.querySelector("body").style.fontSize = "2em";
+    document.querySelectorAll(".device_container").forEach((el) => {
+      el.style.gridTemplateRows = "1fr auto";
+      el.style.gridTemplateColumns = "1fr";
+    });
   } else {
     document.querySelector("body").style.fontSize = "1em";
+    document.querySelectorAll(".device_container").forEach((el) => {
+      el.style.gridTemplateRows = "1fr";
+      el.style.gridTemplateColumns = "1fr auto";
+    });
   }
 }
