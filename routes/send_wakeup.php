6ff70db2d1d8a9d2f996e6f2e24a2d4cdb5ef1c0
Elemer <iiasanmi9@gmail.com>
2023-11-30 17:17:00 +0100

    wakeup API set up

---
+++ routes/send_wakeup.php
@@ -0,0 +1,36 @@
+<?php
+
+$wakeup_id = $_GET['id'];
+
+require '../util/db.php';
+$link = db_connect();
+require '../util/queries.php';
+
+$wakeup_device = get_wakeup_device($link, $wakeup_id);
+$mac_address = $wakeup_device['mac_address'];
+
+$device = get_device($link, $wakeup_device['device']);
+$device_ip = $device['ip'];
+
+$url = 'http://' . $device_ip . '/' . 'tools/wake.php?addr=' . $mac_address;
+
+$status = 'OK!';
+http_response_code(200);
+$ctx = stream_context_create(
+    array(
+        'http' => array(
+            'timeout' => 5
+        )
+    )
+);
+$response = file_get_contents($url, 0, $ctx);
+
+if (!$response) {
+    $status = 'Error!';
+    http_response_code(503);
+}
+
+echo $status;
+exit();
+
+?>
--- util/queries.php
+++ util/queries.php
@@ -1,4 +1,15 @@
 <?php
+
+function get_device($link, $device_id)
+{
+    $query = 'SELECT * FROM carmesyes_devices WHERE `id`=?';
+    $request = $link->prepare($query);
+    $request->bind_param('s', $device_id);
+    $request->execute();
+    $result = $request->get_result();
+
+    return mysqli_fetch_all($result, MYSQLI_ASSOC)[0];
+}
 function get_device_pinglog($link, $device_id, $limit = 100)
 {
     $query = 'SELECT * FROM carmesyes_pinglog WHERE `device`=?';
@@ -22,5 +33,16 @@ function get_device_wakeup_devices($link, $device_id)
     $result = $request->get_result();
 
     return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+function get_wakeup_device($link, $wakeup_id)
+{
+    $query = 'SELECT * FROM carmesyes_wakeup WHERE `id`=?';
+    $request = $link->prepare($query);
+    $request->bind_param('s', $wakeup_id);
+    $request->execute();
+    $result = $request->get_result();
+
+    return mysqli_fetch_all($result, MYSQLI_ASSOC)[0];
 }
 ?>
