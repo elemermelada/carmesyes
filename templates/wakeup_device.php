5324a541e2efaa4b68fc61e1989321a5b1d34348
Elemer <iiasanmi9@gmail.com>
2023-11-30 17:45:57 +0100

    more slick

--- templates/wakeup_device.php
+++ templates/wakeup_device.php
@@ -13,7 +13,7 @@
             <button hx-get="routes/send_wakeup.php?id=<?php echo $wakeup_device['id']; ?>"
                 hx-target="#wakeup_status_<?php echo $wakeup_device['id']; ?>" hx-swap="innerHTML"
                 style='font-size:1em;'>
-                Send wakeup call
+                Wakeup
                 <span id='wakeup_status_<?php echo $wakeup_device['id']; ?>'></span>
                 <img class="htmx-indicator" width='10px'
                     src="https://www.superiorlawncareusa.com/wp-content/uploads/2020/05/loading-gif-png-5.gif">
