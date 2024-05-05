9beeb32647561872986e56aa3e1d2bf18d2040bb
elemermelada <iiasanmi9@gmail.com>
2024-05-05 15:31:03 +0200

    create new route

---
+++ routes/proxy_request.php
@@ -0,0 +1,22 @@
+<?php
+$device_id = $_GET['id'];
+$port = $_GET['port'];
+//$path = $_GET['path'];
+
+require '../util/db.php';
+$link = db_connect();
+
+require '../util/queries.php';
+$device = get_device($link, $device_id);
+
+$url = "http://" . $device['ip'] . ":$port/?"
+
+foreach($_GET as $key => $value){
+    $url .= $key . "=" . $value . "&";
+}
+
+$result = file_get_contents($url);
+
+$output = array("url"=>$url, "result"=>$result);
+echo json_encode($output);
+?>
