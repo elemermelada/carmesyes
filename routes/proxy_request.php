0d73a1f7c21d703271f3c9c2a0925c16b7f41522
elemermelada <iiasanmi9@gmail.com>
2024-05-05 15:35:02 +0200

    tests

--- routes/proxy_request.php
+++ routes/proxy_request.php
@@ -1,11 +1,13 @@
 <?php
+echo "1";
 $device_id = $_GET['id'];
 $port = $_GET['port'];
 //$path = $_GET['path'];
-
+echo "2";
 require '../util/db.php';
 $link = db_connect();
 
+echo "3";
 require '../util/queries.php';
 $device = get_device($link, $device_id);
 
@@ -15,7 +17,9 @@ foreach($_GET as $key => $value){
     $url .= $key . "=" . $value . "&";
 }
 
+echo "4";
 $result = file_get_contents($url);
+echo "5";
 
 $output = array("url"=>$url, "result"=>$result);
 echo json_encode($output);
